50676f9fce99ba2071a12322c83a56a837d933fe
vinay <912vinay@gmail.com>
2024-07-14 15:12:08 +0530

    Added Junits

---
+++ src/main/java/com/vinaypal/kafka/test/KafkaConsumerTest.java
@@ -0,0 +1,31 @@
+package com.vinaypal.kafka.test;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Component
+@Slf4j
+@Data
+public class KafkaConsumerTest {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+    private String payload;
+
+    @KafkaListener(topics = "${test.topic}")
+    public void receive(ConsumerRecord<?, ?> consumerRecord) {
+        log.info("received payload='{}'", consumerRecord.toString());
+        payload = consumerRecord.toString();
+        latch.countDown();
+    }
+
+    public void resetLatch() {
+        latch = new CountDownLatch(1);
+    }
+
+    // other getters
+}
